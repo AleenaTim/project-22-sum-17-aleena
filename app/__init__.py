e9931e90a516e896d385e7654aa6b452886c4799
aleena.t <aleenatim@gmail.com>
2022-06-22 17:55:16 -0400

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306 )
 print(mydb)
 
+
 class TimelinePost(Model):
     name = CharField()
     email = CharField()
@@ -22,8 +23,9 @@ class TimelinePost(Model):
     class Meta:
         database = mydb
 
-mydb.connect()
-mydb.create_tables([TimelinePost])
+
+# mydb.connect()
+# mydb.create_tables([TimelinePost])
 
 
 
