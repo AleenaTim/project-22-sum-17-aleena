54dd38b165247cd75696c86b421868601d69a4c9
aleena.t <aleenatim@gmail.com>
2022-06-22 17:25:27 -0400

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, json,  url_for, request, redirect
 from app.map_app import map_app
 from peewee import *
+import datetime
 import folium
 import os
 import json
@@ -10,6 +11,21 @@ app = Flask(__name__)
 
 mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306 )
 print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+        
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+
+
 app.register_blueprint(map_app)
 #dataFile = open("/root/project-22-sum-17-aleena-emily-zareen/app/static/data.json" , encoding = "utf-8")
 dataFile = open("app/static/data.json" , encoding = "utf-8")
