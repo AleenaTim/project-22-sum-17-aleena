783b98ef837c1c3e817d83b010f08535da72938c
aleena.t <aleenatim@gmail.com>
2022-06-22 21:09:33 -0400

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json,  url_for, request, redirect
+from flask import Flask, render_template, json,  url_for, request, redirect
 from app.map_app import map_app
 from peewee import *
 from playhouse.shortcuts import model_to_dict
