088d294bd9ca64eaa6a2d7d3cc1a6b919b66be97
aleena.t <aleenatim@gmail.com>
2022-06-27 03:14:13 -0400

    configured systemd service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-tmux kill-session
-
 cd /root/project-22-sum-17-aleena
 
 git fetch && git reset origin/main --hard
@@ -10,8 +8,8 @@ source "/root/project-22-sum-17-aleena/python3-virtualenv/bin/activate"
 
 pip install -r requirements.txt
 
-NEW_TMUX="redeploy"
-
 RUN="flask run --host=0.0.0.0"
 
-tmux new-session -d -s "$NEW_TMUX" "$RUN"
+systemctl daemon-reload
+
+systemctl restart myportfolio
