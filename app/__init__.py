c2232f71a72be306079fe288518dc0a655160023
aleena.t <aleenatim@gmail.com>
2022-06-27 04:48:25 -0400

    timeline form update

--- app/__init__.py
+++ app/__init__.py
@@ -54,7 +54,8 @@ def hobbies():
  
 @app.route('/timeline')
 def timeline():
-	return render_template('timeline.html', title="Timeline")
+	posts = [model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())]
+	return render_template('timeline.html', title="Timeline", posts=posts)
 
 
 @app.route('/api/timeline_post', methods=['POST'])
